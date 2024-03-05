aca475b3e4335db22f022a71b12c6e568e8551e9
olusola akinsulere <akinsulereolusola@gmail.com>
2024-03-05 19:45:18 +0100

    added views for data ingestion

---
+++ app/accounts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from accounts import views
+
+
+urlpatterns = [
+    path('ingest_csv_from_url/', views.ingest_csv_from_url, name='ingest_data')
+]
--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+"""
+Views for creating data
+"""
+import os
+from django.http import JsonResponse
+from django.core.management import call_command
+from django.views.decorators.http import require_http_methods
 
-# Create your views here.
+@require_http_methods(["GET"])
+def ingest_csv_from_url(request):
+    csv_url = os.getenv('CSV_URL')
+    if not csv_url:
+        return JsonResponse({'error': 'CSV_URL environment variable is not set'}, status=400)
+    try:
+        call_command('data_ingestion', csv_url=csv_url)
+        return JsonResponse({'success': 'Data ingestion initiated successfully.'})
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
