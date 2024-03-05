2edd57e22ac4e32207454a6f67c88542b90b1c30
olusola akinsulere <akinsulereolusola@gmail.com>
2024-03-05 14:37:44 +0100

    wrote tests for creating client

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -3,3 +3,13 @@ Django test to test models
 """
 
 from django.test import TestCase
+from core import models
+from django.core.exceptions import ValidationError
+
+
+class ClientModelTest(TestCase):
+    def test_client_creation(self):
+        client = Client.objects.create(name="Test Client")
+        self.assertEqual(client.name, "Test Client")
+        self.assertTrue(client.created_at)
+        self.assertTrue(client.updated)
